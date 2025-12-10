ea59006ebecbc35a8b5edc29e6777af504b08d34
Apoyo Sistemas <apoyosistemas@caj.org.pe>
2025-12-10 13:10:31 -0500

    Arreglo menor de Logo en Buscar.php

--- buscar.php
+++ buscar.php
@@ -155,7 +155,7 @@ function labelize($key, $pretty){
   <main class="hero-wrap">
     <div class="hero">
       <?php
-        $logo = 'assets/';
+        $logo = 'assets/logo.png';
         if (file_exists($logo)) {
           echo '<img class="hero-logo" src="'.$logo.'" alt="Ilustre Colegio de Abogados de Junín" />';
         }
